26954795b8f7297322f1f4f4250be6b6e096fff0
Marx Bitware <info@marxbitware.co.uk>
2021-02-27 19:44:20 +0700

    Add project files.

---
+++ LibCodeGroup/CodeGroup.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+
+namespace LibCodeGroup
+{
+    /// <summary>
+    /// CodeGroup converts a byte array (usually ciphertext) to a string of text as defined in the lookup.
+    /// </summary>
+    public static class CodeGroup
+    {
+        private static string[] lookup = new string[]
+        {
+            "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ",
+            "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ",
+            "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ", "CK", "CL", "CM", "CN", "CO", "CP", "CQ", "CR", "CS", "CT", "CU", "CV", "CW", "CX", "CY", "CZ",
+            "DA", "DB", "DC", "DD", "DE", "DF", "DG", "DH", "DI", "DJ", "DK", "DL", "DM", "DN", "DO", "DP", "DQ", "DR", "DS", "DT", "DU", "DV", "DW", "DX", "DY", "DZ",
+            "EA", "EB", "EC", "ED", "EE", "EF", "EG", "EH", "EI", "EJ", "EK", "EL", "EM", "EN", "EO", "EP", "EQ", "ER", "ES", "ET", "EU", "EV", "EW", "EX", "EY", "EZ",
+            "FA", "FB", "FC", "FD", "FE", "FF", "FG", "FH", "FI", "FJ", "FK", "FL", "FM", "FN", "FO", "FP", "FQ", "FR", "FS", "FT", "FU", "FV", "FW", "FX", "FY", "FZ",
+            "GA", "GB", "GC", "GD", "GE", "GF", "GG", "GH", "GI", "GJ", "GK", "GL", "GM", "GN", "GO", "GP", "GQ", "GR", "GS", "GT", "GU", "GV", "GW", "GX", "GY", "GZ",
+            "HA", "HB", "HC", "HD", "HE", "HF", "HG", "HH", "HI", "HJ", "HK", "HL", "HM", "HN", "HO", "HP", "HQ", "HR", "HS", "HT", "HU", "HV", "HW", "HX", "HY", "HZ",
+            "IA", "IB", "IC", "ID", "IE", "IF", "IG", "IH", "II", "IJ", "IK", "IL", "IM", "IN", "IO", "IP", "IQ", "IR", "IS", "IT", "IU", "IV", "IW", "IX", "IY", "IZ",
+            "JA", "JB", "JC", "JD", "JE", "JF", "JG", "JH", "JI", "JJ", "JK", "JL", "JM", "JN", "JO", "JP", "JQ", "JR", "JS", "JT", "JU", "JV"
+        };
+
+        public static string ConvertToCode(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+                sb.Append(lookup[(int)data[i]]);
+
+            return sb.ToString();
+        }
+
+        public static byte[] ConvertFromCode(string data)
+        {
+            byte[] decoded = new byte[data.Length / 2];
+            int index = 0;
+
+            for (int i = 0; i < decoded.Length; i++)
+                decoded[i] = (byte)FindIndex(data[index++].ToString() + data[index++].ToString());
+                
+            return decoded;
+        }
+
+        private static int FindIndex(string data)
+        {
+            for(int i = 0; i < lookup.Length; i++)
+                if (lookup[i] == data)
+                    return i;
+
+            return -1;
+        }
+    }
+}
---
+++ VarnamCodeGroup/Program.cs
@@ -0,0 +1,27 @@
+﻿using LibCodeGroup;
+using LibVernam;
+using System;
+using System.Text;
+
+namespace VarnamCodeGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToBeEncrypted = "This is a string of text.";
+            byte[] bytesToBeEncrypted = Encoding.ASCII.GetBytes(textToBeEncrypted);
+            byte[] key = Helpers.GenerateRandomBytes(bytesToBeEncrypted.Length);
+
+            byte[] cipherText = Vernam.Encrypt(bytesToBeEncrypted, key);
+
+            string codedFile = CodeGroup.ConvertToCode(cipherText);
+            byte[] decodefile = CodeGroup.ConvertFromCode(codedFile);
+
+
+            string textDeciphered = Encoding.ASCII.GetString(Vernam.Decrypt(decodefile, key));
+
+            Console.WriteLine(textDeciphered);
+        }
+    }
+}
